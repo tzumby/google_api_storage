f4b5eda671853d142a04e948b8c214e9190bf33d
Razvan Draghici <rdraghici@weedmaps.com>
2019-08-10 12:47:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# GoogleApi.Storage.V1
+
+Stores and retrieves potentially large, immutable data objects.
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `google_api_storage` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [{:google_api_storage, "~> 0.0.2"}]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/google_api_storage](https://hexdocs.pm/google_api_storage).
---
+++ mix.exs
@@ -0,0 +1,49 @@
+defmodule GoogleApi.Storage.V1.Mixfile do
+  use Mix.Project
+
+  @version "0.9.0"
+
+  def project() do
+    [
+      app: :google_api_storage,
+      version: @version,
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
+      deps: deps(),
+      source_url: "https://github.com/GoogleCloudPlatform/elixir-google-api/tree/master/clients/storage"
+    ]
+  end
+
+  def application() do
+    [extra_applications: [:logger]]
+  end
+
+  defp deps() do
+    [
+      {:google_gax, "~> 0.1.0"},
+      {:ex_doc, "~> 0.16", only: :dev},
+      {:goth, "~> 0.4.0", only: [:dev, :test]}
+    ]
+  end
+
+  defp description() do
+    """
+    Stores and retrieves potentially large, immutable data objects.
+    """
+  end
+
+  defp package() do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE"],
+      maintainers: ["Jeff Ching"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/GoogleCloudPlatform/elixir-google-api/tree/master/clients/storage",
+        "Homepage" => "https://cloud.google.com/storage/"
+      }
+    ]
+  end
+end
